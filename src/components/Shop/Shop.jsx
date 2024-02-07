0ff700b306921444a45a92fd5605717e471d349c
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2024-02-06 23:41:12 -0800

    Added next and prev page buttons

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,10 +4,12 @@ import Card from '../Cards/Card';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 const Shop = () =>{
-    console.log()
+
     const KEY = "43fcf1bb9e6a4fbe98ceb93a4c7de24e";
     const [responseData, setResponseData] = useState([]);
     const [showData, setShowData] = useState(true);
+    const [nextPage, setNextPage] = useState('');
+    const [prevPage, setPrevPage] = useState('');
     useEffect(() => {
         const elem = document.body;
         const getData = async () => {
@@ -17,16 +19,55 @@ const Shop = () =>{
           );          
           elem.style = 'height:100%';
           setResponseData(response.data.results);
+          setNextPage(response.data.next);
+          setPrevPage(response.data.previous);
         };        
-        getData();          
+        getData();       
+
 
         return (() =>{ elem.style = 'height:100%vh'})
         
       }, []);
+      
+      const goToNextPage = async() =>{
+        const elem = document.body;
+        const getData = async () => {
+          console.log('Fetched data.')          
+          const response = await axios.get(
+            nextPage,
+          );          
+          elem.style = 'height:100%';
+          setResponseData(response.data.results);
+          console.log(response.data)
+          setNextPage(response.data.next);
+          setPrevPage(response.data.previous)
+        };        
+        getData();    
+        window.scrollTo({top: 0})
+
+      }
+
+      const goToPreviousPage = async() =>{
+        const elem = document.body;
+        const getData = async () => {
+          console.log('Fetched data.')          
+          const response = await axios.get(
+            prevPage,
+          );          
+          elem.style = 'height:100%';
+          setResponseData(response.data.results);
+          console.log(response.data)
+          setNextPage(response.data.next);
+          setPrevPage(response.data.previous)
+        };        
+        getData();    
+        window.scrollTo({top: 0})
+      }
+  
 
       
 
-  
+      
 
 
     return (
@@ -59,6 +100,14 @@ const Shop = () =>{
                 </Link>)}
                 ):<h1>Unfortunately the API does not provide this Data the left bar is just a proof of concept</h1>
             }
+            <div className="prev-btn-container span-prev">
+              <button onClick={goToPreviousPage} className='navigation-btns'>Previous page</button>
+            </div>
+            <div className="next-btn-container span-2">
+              <button onClick={goToNextPage} className='navigation-btns'>Next page</button>
+            </div>
+            {/* <button onClick={goToNextPage} className='navigation-btns span-2'>Next page</button> */}
+
           </div>
         </>
     )
